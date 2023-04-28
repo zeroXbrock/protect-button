8e8ff7e97d697f99f995ba26d57a6da4d1fdf6db
Brock Smedley <2791467+zeroXbrock@users.noreply.github.com>
2023-04-28 07:19:25 -0700

    (sin) include build dir for npm release; should revert later and do this properly w CI

---
+++ protect/build/index.d.ts
@@ -0,0 +1,4 @@
+import FlashbotsProtectButton from './button';
+export { HintPreferences, ProtectButtonOptions } from './button';
+export default FlashbotsProtectButton;
+//# sourceMappingURL=index.d.ts.map
